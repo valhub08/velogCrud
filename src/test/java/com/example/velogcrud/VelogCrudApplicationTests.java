64ae1ac8d6bcb42eb8b33b024aeedddd22e03f3c
valhub08 <jhstl1203@gmail.com>
2025-01-20 00:15:48 +0900

    feat :: VelogCrudApplicationTests 추가

---
+++ src/test/java/com/example/velogcrud/VelogCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.velogcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
