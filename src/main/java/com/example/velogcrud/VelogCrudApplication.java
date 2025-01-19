5884bf9c2e2c3e8b271d638fa2e4b3b386d1aeb5
valhub08 <jhstl1203@gmail.com>
2025-01-19 15:32:45 +0900

    first :: commit message

---
+++ src/main/java/com/example/velogcrud/VelogCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.velogcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelogCrudApplication.class, args);
+    }
+
+}
